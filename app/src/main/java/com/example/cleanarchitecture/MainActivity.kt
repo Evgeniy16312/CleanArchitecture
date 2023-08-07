0acb37478d501929ca4e9e7719f34569f110a579
AXBIT\etyurin <etyurin@axbit.ru>
2023-08-07 15:51:18 +0400

    Lesson 1-3, до исправления repository

--- app/src/main/java/com/example/cleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.cleanarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cleanarchitecture.data.UserRepositoryImpl
+import com.example.cleanarchitecture.databinding.ActivityMainBinding
+import com.example.cleanarchitecture.domain.models.SaveUserNameParam
+import com.example.cleanarchitecture.domain.models.UserName
+import com.example.cleanarchitecture.domain.repository.UserRepository
+import com.example.cleanarchitecture.domain.usecase.GetUserNameUseCase
+import com.example.cleanarchitecture.domain.usecase.SaveUserNameUseCase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val userRepository by lazy { UserRepositoryImpl(context = applicationContext) }
+    private val getUserNameUseCase by lazy { GetUserNameUseCase(userRepository = userRepository) }
+    private val saveUserNameUseCase by lazy { SaveUserNameUseCase(userRepository = userRepository) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonSave.setOnClickListener {
+            val text = binding.editTv.text.toString()
+            val params = SaveUserNameParam(name = text)
+            val result: Boolean = saveUserNameUseCase.execute(param = params)
+            binding.textView.text = "Save result = ${result}"
+        }
+
+        binding.buttonGet.setOnClickListener {
+            val userName: UserName = getUserNameUseCase.execute()
+            binding.textView.text = "${userName.firstName} ${userName.lastName}"
+        }
     }
 }
---
+++ app/src/main/java/com/example/cleanarchitecture/domain/models/SaveUserNameParam.kt
@@ -0,0 +1,5 @@
+package com.example.cleanarchitecture.domain.models
+
+class SaveUserNameParam(
+    val name: String
+)
